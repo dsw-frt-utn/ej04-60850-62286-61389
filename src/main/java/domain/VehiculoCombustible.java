b00ce15df1bc34e2e9ca110e88a3aeecb6bfd1de
Sebastian Ferrari Osado <OsadoSebastianFerrari@alu.frt.utn.edu.ar>
2026-04-16 17:38:54 -0300

    creacion de clase marca

--- src/main/java/domain/VehiculoCombustible.java
+++ src/main/java/domain/VehiculoCombustible.java
@@ -6,13 +6,13 @@ public class VehiculoCombustible extends Vehiculo {
     private double kilometrosPorLitro;
     private double litrosExtra;
 
-    public VehiculoCombustible(String patente, String marca, String modelo, int anio, double capacidadCarga,
+    public VehiculoCombustible(String patente, Marca marca, String modelo, int anio, double capacidadCarga,
                                Sucursal sucursal, double kilometrosPorLitro, double litrosExtra) {
-        super(VehiculoTipo.COMBUSTIBLE, patente, marca, modelo, anio, capacidadCarga, sucursal);
+        super(VehiculoTipo.COMBUSTIBLE, patente, marca,modelo, anio, capacidadCarga, sucursal);
         this.kilometrosPorLitro = kilometrosPorLitro;
         this.litrosExtra = litrosExtra;
     }
-    
+       
      public double getKilometrosPorLitro() {
         return kilometrosPorLitro;
     }
